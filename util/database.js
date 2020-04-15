21f10ad706f2bfac58856a0bb87aa53e68d46c80
User <user@MacBook-User.local>
2020-04-15 10:34:59 +0300

    database updated

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '81234567'
+const sequelize = new Sequelize('node-complete', 'root', '81234567', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
